d03e3a302881a13ae07170154ba0828e7cfe267d
Faizan Jan <faizan@equip.co>
2023-09-15 23:18:17 +0530

    Make the custom dropdown with search option in skills

--- src/Icons/XMark.jsx
+++ src/Icons/XMark.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const XMark = ({classes, props}) => {
+const XMark = ({classes}) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
--- src/app/Components/Skills.jsx
+++ src/app/Components/Skills.jsx
@@ -1,5 +1,6 @@
 import XMark from "@/Icons/XMark";
 import { skills } from "@/skills";
+import { useState, useRef } from "react";
 
 const Skills = ({ skillType, skillSet, updateSkill }) => {
     const colorScheme = {
@@ -7,15 +8,18 @@ const Skills = ({ skillType, skillSet, updateSkill }) => {
         suggested: "blue",
         optional: "yellow",
     };
+    const [showDropDown, setShowDropDown] = useState(false);
+    const [filter, setFilter] = useState("");
+    const filteredSkills = skills.filter((skill) => skill.name.toLowerCase().includes(filter.toLowerCase()));
 
     return (
         <div className="my-1 pl-5">
             <h1 className={`text-lg font-bold capitalize text-${colorScheme[skillType]}-900`}>{skillType} Skills</h1>
-            <div className="flex flex-row gap-x-3 mt-2 flex-wrap justify-start">
+            <div className="flex flex-row gap-x-3 mt-2 flex-wrap justify-start items-center">
                 {skillSet[skillType].map((skillId) => (
                     <span
                         key={skillId}
-                        className={`flex flex-row items-center justify-start rounded-full px-3 py-1 text-sm font-semibold border mr-2 mb-2 bg-${colorScheme[skillType]}-200/50 text-${colorScheme[skillType]}-900 border-${colorScheme[skillType]}-900`}
+                        className={`flex flex-row items-center justify-start rounded-full px-3 py-1 text-sm font-semibold border mr-2 bg-${colorScheme[skillType]}-200/50 text-${colorScheme[skillType]}-900 border-${colorScheme[skillType]}-900`}
                     >
                         {skills.find((skillSet) => skillSet.id == skillId).name}
                         <span
@@ -27,26 +31,48 @@ const Skills = ({ skillType, skillSet, updateSkill }) => {
                         </span>
                     </span>
                 ))}
-                <select
-                    className={`flex flex-row items-center justify-start rounded-full px-3 py-1 text-sm font-semibold border mr-2 mb-2 w-40 text-${colorScheme[skillType]}-900 border-${colorScheme[skillType]}-900 `}
-                    onChange={(e) => {
-                        updateSkill(e.target.value, skillType, "add");
-                        e.target.value = "";
+
+                {/* Custom Dropdown with filter */}
+                <div
+                    className={"w-64 " + (showDropDown ? "relative" : "")}
+                    onFocus={() => {
+                        setShowDropDown(true);
                     }}
+                    onBlur={() => setShowDropDown(false)}
                 >
-                    <option value="" selected disabled>
-                        Add More Skills
-                    </option>
-                    {skills.map((skill) => (
-                        <option
-                            key={skill.id}
-                            value={skill.id}
-                            disabled={Object.values(skillSet).flat().includes(skill.id)}
-                        >
-                            {skill.name}
-                        </option>
-                    ))}
-                </select>
+                    <input
+                        type="text"
+                        className={"border py-1 px-4 w-full rounded-md"}
+                        placeholder="Add Skill..."
+                        onChange={(e) => setFilter(e.target.value)}
+                        value={filter}
+                    />
+                    {filter && (
+                        <ul className="absolute backdrop-blur-lg shadow-xl border  max-h-[400px] w-full overflow-scroll">
+                            {filteredSkills.map((skill) => (
+                                <li
+                                    key={skill.id}
+                                    className={
+                                        `relative z-100 text-neutral-700 px-4 py-2 truncate` +
+                                        (Object.values(skillSet).flat().includes(skill.id)
+                                            ? " opacity-50 cursor-not-allowed"
+                                            : "  hover:bg-neutral-200/60 hover:translate-x-0.5  cursor-pointer")
+                                    }
+                                    title={skill.name}
+                                    onMouseDown={(e) => {
+                                        e.stopPropagation();
+                                        if (Object.values(skillSet).flat().includes(skill.id)) return;
+                                        updateSkill(skill.id, skillType, "add");
+                                        setFilter("");
+                                    }}
+                                    disabled={Object.values(skillSet).flat().includes(skill.id)}
+                                >
+                                    {skill.name}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
             </div>
         </div>
     );

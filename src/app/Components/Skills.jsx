7a5db21e97a63dc7a6564fa3f46cad3207cc7854
Faizan Jan <faizan@equip.co>
2023-09-15 15:01:32 +0530

    WIP

---
+++ src/app/Components/Skills.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import XMark from "@/Icons/XMark";
+import { skills } from "@/skills";
+
+const Skills = ({ skillType, skillSet, removeSkill, addSkill }) => {
+    const colorScheme = {
+        relevant: "green",
+        suggested: "blue",
+        optional: "yellow",
+    };
+
+    return (
+        <div className="my-5 pl-5">
+            <h1 className={`text-xl font-bold capitalize text-${colorScheme[skillType]}-900`}>{skillType} Skills</h1>
+            <div className="flex flex-row gap-x-3 mt-3 flex-wrap justify-start">
+                {skillSet[skillType].map((skillId) => (
+                    <span
+                        key={skillId}
+                        className={`flex flex-row items-center justify-start rounded-full px-3 py-1 text-sm font-semibold border mr-2 mb-2 bg-${colorScheme[skillType]}-200 text-${colorScheme[skillType]}-900 border-${colorScheme[skillType]}-900`}
+                    >
+                        {skills.find((skillSet) => skillSet.id == skillId).name}
+                        <span
+                            onClick={() => {
+                                removeSkill(skillId, skillType);
+                            }}
+                        >
+                            <XMark classes={"w-4 h-4 font-bold ml-2 cursor-pointer"} />
+                        </span>
+                    </span>
+                ))}
+                <select
+                        className={`flex flex-row items-center justify-start rounded-full px-3 py-1 text-sm font-semibold border mr-2 mb-2 w-max text-${colorScheme[skillType]}-900 border-${colorScheme[skillType]}-900`}
+                    onChange={(e) => {
+                        addSkill(e.target.value, skillType);
+                        e.target.value = "";
+                    }}
+                >
+                    <option value="" selected disabled>
+                        Add More Skills
+                    </option>
+                    {skills.map((skill) => (
+                        <option
+                            key={skill.id}
+                            value={skill.id}
+                            disabled={skillSet[skillType].includes(parseInt(skill.id))}
+                        >
+                            {skill.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,61 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
-  return (
-    <main>
-      Hello world
-    </main>
-  )
-}
+import { useState, useEffect } from "react";
+import { roles } from "@/roles";
+import { skills } from "@/skills";
+import { map } from "@/mapping";
+
+import Skills from "./Components/Skills";
+
+const page = () => {
+    const [role, setRole] = useState(roles[0]);
+    const [skillSet, setSkillSet] = useState(map[role?.id]);
+
+    useEffect(() => {
+        setSkillSet(map[role?.id]);
+    }, [role]);
+
+    const removeSkill = (skillId, skillType) => {
+        setSkillSet({
+            ...skillSet,
+            [skillType]: skillSet[skillType].filter((id) => id !== skillId),
+        });
+    };
+
+    const addSkill = (skillId, skillType) => {
+        setSkillSet({
+            ...skillSet,
+            [skillType]: [...skillSet[skillType], skillId],
+        });
+    };
+
+    return (
+        <div className="w-10/12 mx-auto my-10 p-5 border rounded-md flex flex-col gap-y-5">
+            <div className="flex flex-row items-center gap-x-8 mb-5">
+                <h1 className="text-2xl font-bold">Role:</h1>
+                <select
+                    className="p-2 border rounded-md"
+                    onChange={(e) => setRole(roles.find((role) => role.id === e.target.value))}
+                >
+                    {roles.map((role) => (
+                        <option key={role.id} value={role.id}>
+                            {role.title}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            {Object.keys(skillSet).map((skillType) => (
+                <Skills
+                    skillType={skillType}
+                    skillSet={skillSet}
+                    removeSkill={removeSkill}
+                    addSkill={addSkill}
+                    // skills={skills}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default page;
